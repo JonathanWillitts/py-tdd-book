630653bf1bce5ad978576003ba79abfe2487787c
JonathanWillitts <JonathanWillitts@users.noreply.github.com>
2016-08-30 09:24:52 +0100

    Removed commented out copy-pasted code.

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,3 @@
-#from selenium import webdriver
-
-#browser = webdriver.Firefox()
-#browser.get('http://localhost:8000')
-
-#assert 'Django' in browser.title
 from selenium import webdriver
 
 browser = webdriver.Firefox()
