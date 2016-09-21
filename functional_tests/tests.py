d51ddc543cc24e77e867c7a4d7636cb9705e0a16
JonathanWillitts <JonathanWillitts@users.noreply.github.com>
2016-09-21 09:19:09 +0100

    Minor comment modifications (test commit).

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,24 +88,24 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
-        # Francis gets his own unique URL
+        # Francis gets his own unique URL.
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # Again, there is no trace of Edith's list
+        # Again, there is no trace of Edith's list.
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        # Satisfied, she goes back to sleep
+        # Satisfied, she goes back to sleep.
     
     def test_layout_and_styling(self):
-        # Edith goes to the home page
+        # Edith goes to the home page.
         self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
-        # She notices the input box is nicely centered
+        # She notices the input box is nicely centered.
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
