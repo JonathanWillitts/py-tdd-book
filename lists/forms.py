b42f675b942995f6fce801644c2f50f77c040c5c
JonathanWillitts <JonathanWillitts@users.noreply.github.com>
2016-09-30 15:13:06 +0100

    Changes to use ExistingListItemFrom in the List View. FTs now passing!

--- lists/forms.py
+++ lists/forms.py
@@ -27,6 +27,7 @@ class ItemForm(forms.models.ModelForm):
 
 
 class ExistingListItemForm(ItemForm):
+
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args,**kwargs)
         self.instance.list = for_list
@@ -37,4 +38,7 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+    
+    def save(self):
+        return forms.models.ModelForm.save(self)
 
